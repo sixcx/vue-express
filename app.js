3b90832cb680686cd9af3450dfd2c351579bbe82
sixcx <sixcx0202@126.com>
2018-07-31 18:06:57 +0800

    add express

---
+++ app.js
@@ -0,0 +1,11 @@
+//express配置文件
+const express = require('express');
+const app = express();
+
+app.use('/', (req, res) => {
+  res.send('hello world');
+})
+
+app.listen(3000, () => {
+  console.log('app listening on port 3000');
+})
